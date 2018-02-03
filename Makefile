7f8a86c17129ed5910ae4bd260536c0a96ad6929
alexeypant <alexeypant@gmail.com>
2018-02-03 16:32:42 +0300

    reassamble the project

---
+++ Makefile
@@ -0,0 +1,29 @@
+install: 
+	npm install
+
+start: 
+		npm run babel-node -- src/bin/brain-games.js
+
+start-even: 
+		npm run babel-node -- src/bin/brain-even.js
+
+start-calc:
+		npm run babel-node -- src/bin/brain-calc.js
+
+start-gcd:
+		npm run babel-node -- src/bin/brain-gcd.js
+
+start-balance:
+		npm run babel-node -- src/bin/brain-balance.js
+
+start-progression:
+		npm run babel-node -- src/bin/brain-progression.js
+
+start-prime:
+		npm run babel-node -- src/bin/brain-prime.js
+
+publish:
+	npm publish
+
+lint:
+	npm run eslint  src/
---
+++ index.js
@@ -0,0 +1,56 @@
+// I need to add logic description here
+import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
+
+const getResponseAndShow = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`'Hello, ${userName}!'`);
+  return userName;
+};
+
+const getGuess = () => {
+  const guess = readlineSync.question('Your answer: ');
+  return guess;
+};
+
+const checkAnswer = (guess, correctAnswer) => {
+  if (guess !== String(correctAnswer)) {
+    console.log(`'${guess}'' was wrong answer ;(. Correct answer was  '${correctAnswer}'`);
+    return false;
+  }
+  console.log('Correct!');
+  return true;
+};
+
+export const getGCD = (a, b) => {
+  if (b === 0) {
+    return (a < 0) ? -a : a;
+  }
+  return getGCD(b, a % b);
+};
+
+export const runGame = (game, message) => {
+  console.log(message);
+  console.log(`${game.task}`);
+  const playerName = getResponseAndShow(); // getting the name, saying hello and show the name
+  let quiz;
+  let guess;
+  let isCorrect;
+  let i = 0;
+  while (i < 3) {
+    quiz = game.function();
+    console.log(`Question: ${car(quiz)}`);
+    guess = getGuess();
+    isCorrect = checkAnswer(guess, cdr(quiz));
+    if (!isCorrect) {
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations ${playerName}!`);
+      break;
+    }
+    i += 1;
+  }
+};
+
+export const getRoundRandomNumber = () => Math.round(Math.random() * 100);
